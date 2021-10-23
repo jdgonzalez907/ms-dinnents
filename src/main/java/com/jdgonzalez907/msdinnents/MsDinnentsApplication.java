84ec7a0798d283226552a2beabb65353b33287de
Juan David Gonzalez Bedoya <jdgonzalez907@gmail.com>
2021-10-22 20:23:27 -0500

    Start project

---
+++ src/main/java/com/jdgonzalez907/msdinnents/MsDinnentsApplication.java
@@ -0,0 +1,13 @@
+package com.jdgonzalez907.msdinnents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDinnentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsDinnentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jdgonzalez907/msdinnents/MsDinnentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdgonzalez907.msdinnents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsDinnentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
